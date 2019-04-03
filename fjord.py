75d3bc713109436a96d0dda9c9bdd83829c27190
James Stoup <jrstoup@gmail.com>
2019-04-03 16:41:15 -0400

    Initial commit
    Getting the ball rolling on my nordvpn wrapper.

---
+++ fjord.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+print 'welcome to fjord'
+
+
+#openvpn nl308.nordvpn.com.tcp.ovpn
+
+### TO DO ###
+#
+#   Features
+# 
+#    - continuously running app in terminal
+#    - shows connection status + stats
+#    - shows what country you are really in
+#    - shows what country the internet thinks you are in
+#    - supports all of the options that the official app has
+#    - allows you to save a configuration
+#    - relies on openvpn
+#    - shows map of world in ascii (if we are feeling frisky)
+#    - tab complete country/city combos
+#    - list explanation of what the damn options mean
+#    - h key shows/hides help
+#    - 
+
+#  GUI
+#
+#    * should change colors based on if we are connected or not
+#    * label showing projected location
+#    * label showing real location
+#    * show list of countries
+#    * show cities per country
+#    * show map of cities (maybe)
+#    * offer to save configurations
+#    * randomize connecting point
+#    * maybe also show connection speed?
