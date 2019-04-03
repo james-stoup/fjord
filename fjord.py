fee06765bd1060d5703c38c3fbac15300add46c7
James Stoup <jrstoup@gmail.com>
2019-04-03 19:37:13 -0400

    Baby steps
    So since this is the first thing I've ever made with
    ncurses and npyscreen it will be slow going until I
    get the hang of things.

--- fjord.py
+++ fjord.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-print 'welcome to fjord'
+import npyscreen
+
+
+print('welcome to fjord')
 
 
 #openvpn nl308.nordvpn.com.tcp.ovpn
@@ -33,3 +36,52 @@ print 'welcome to fjord'
 #    * offer to save configurations
 #    * randomize connecting point
 #    * maybe also show connection speed?
+
+# for use with NordVPN Version 2.2.0-2
+
+
+# during main loop we should just call 'nordvpn status' and change the background
+# color of the main display to red or green depending on the status
+# Status: Disconnected
+
+
+
+
+# Primary class
+class Fjord(npyscreen.NPSAppManaged):
+    def onStart(self):
+        self.addForm("MAIN", MainForm, name='Fjord')
+
+
+# This form class defines the display that will be presented to the user.
+class MainForm(npyscreen.Form):
+    def create(self):
+        npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
+        
+        self.MyTitle = self.add(npyscreen.TitleText, name = "Welcome to Fjord", editable=False )
+        self.ConnectionStatus = self.add(npyscreen.TitleText, name='Status:', value='Disconnected', editable=False, color='DANGER')
+
+        self.nextrely += 1
+        self.RealLoc = self.add(npyscreen.TitleText, name='Real Loc:', value='Home', editable=False)
+        self.ProjLoc = self.add(npyscreen.TitleText, name='Proj Loc:', value='Home', editable=False)
+
+        self.nextrely += 1
+        self.MyColor = self.add(npyscreen.TitleText, name='Color', value='BLUE')
+        self.MyName = self.add(npyscreen.TitleText, name='Name', value='Bob')
+        
+        
+    def afterEditing(self):
+        self.parentApp.setNextForm(None)
+
+        
+def main():
+    """ Welcome to Fjord, a better way of using NordVPN """
+    
+
+
+
+        
+if __name__ == '__main__':
+    fjord = Fjord()
+    fjord.run()
+    
