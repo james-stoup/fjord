14d82e09acc04bb864f417685be2ce9938db5765
James Stoup <jrstoup@gmail.com>
2019-04-04 16:57:13 -0400

    Still can't walk, but we crawl ok
    Still getting the hang of npyscreen. Added some fields that
    don't yet have any callbacks on them as well as added some
    buttons that don't work. So...progress I suppose, albeit slow
    progress.

--- fjord.py
+++ fjord.py
@@ -23,7 +23,8 @@ print('welcome to fjord')
 #    - tab complete country/city combos
 #    - list explanation of what the damn options mean
 #    - h key shows/hides help
-#    - 
+#    - add favorite servers
+#    - list of apps to kill if VPN drops
 
 #  GUI
 #
@@ -51,12 +52,13 @@ print('welcome to fjord')
 class Fjord(npyscreen.NPSAppManaged):
     def onStart(self):
         self.addForm("MAIN", MainForm, name='Fjord')
+#        self.addForm("FORM2", ConnectionForm, name='Connect/Disconnect')
 
 
 # This form class defines the display that will be presented to the user.
 class MainForm(npyscreen.Form):
     def create(self):
-        npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
+#        npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
         
         self.MyTitle = self.add(npyscreen.TitleText, name = "Welcome to Fjord", editable=False )
         self.ConnectionStatus = self.add(npyscreen.TitleText, name='Status:', value='Disconnected', editable=False, color='DANGER')
@@ -66,22 +68,66 @@ class MainForm(npyscreen.Form):
         self.ProjLoc = self.add(npyscreen.TitleText, name='Proj Loc:', value='Home', editable=False)
 
         self.nextrely += 1
-        self.MyColor = self.add(npyscreen.TitleText, name='Color', value='BLUE')
-        self.MyName = self.add(npyscreen.TitleText, name='Name', value='Bob')
+        self.nextrely += 1
+        self.Settings = self.add(npyscreen.TitleText, name = 'Settings', editable=False)
+        self.nextrely += 1
         
+        # prevent editing once connected
+        self.Country = self.add(npyscreen.TitleText, name='Country', value='France')
+        self.City = self.add(npyscreen.TitleText, name='City', value='Paris')
+        self.Protocol = self.add(npyscreen.TitleText, name = 'Protocol', value='UDP')
+
+        # val_list = ["enabled", "disabled"]
+        # self.KillSwitch = self.add(npyscreen.TitleText, name = 'Kill Switch', value='disabled', values=val_list)
+
+        # Options = npyscreen.OptionList()
+        # options = Options.options
+        # options.append(npyscreen.OptionMultiChoice('Multichoice', choices=["enabled", "disabled"]))
+        # self.add(npyscreen.OptionListDisplay, name = 'Kill Switch', values=options, scroll_exit=False, max_height=None)
+        
+        # self.Obfuscate = self.add(npyscreen.TitleText, name = 'Obfuscate', value='disabled')
+        # self.AutoConnect = self.add(npyscreen.TitleText, name = 'Auto Connect', value='disabled')
+        # self.DNS = self.add(npyscreen.TitleText, name = 'DNS', value='disabled')
+        
+        self.KillSwitch = self.add(npyscreen.TitleSelectOne, values=['Enabled', 'Disabled'], name = 'Kill Switch')
+
+        self.nextrely += 1
+#        self.button = self.add(npyscreen.Button, name='Connect', value_changed_callback=self.buttonPress)
+
+    def buttonPress(self, widget):
+        npyscreen.notify_confirm('Button pressed!', title='Woot', wrap=True, wide=True, editw=1)
+        self.parentApp.switchForm('FORM2')
         
     def afterEditing(self):
         self.parentApp.setNextForm(None)
+# END CLASS
+
+
+class ConnectionForm(npyscreen.ActionForm, npyscreen.SplitForm, npyscreen.FormWithMenus):
+    def create(self):
+        self.name = self.add(npyscreen.TitleText, name='Determining status...', editable=False)
+        self.parentApp.setNextForm('MAIN')
+
+    def on_ok(self):
+        npyscreen.notify_confirm("Saved! Going back to main form", title="OK Presed", wrap=True, wide=True, editw=1)
+        self.parentApp.setNextForm('MAIN')
+
+    def on_cancel(self):
+        npyscreen.notify_confirm("NOT Saved, going back to main form", title="OK Presed", wrap=True, wide=True, editw=1)
+        self.parentApp.setNextForm('MAIN') # Back to main form
+# END CLASS
+
 
         
 def main():
     """ Welcome to Fjord, a better way of using NordVPN """
+    fjord = Fjord()
+    fjord.run()
     
 
 
 
         
 if __name__ == '__main__':
-    fjord = Fjord()
-    fjord.run()
-    
+    main()
+    # npyscreen.wrapper(Fjord().run())
